75e526f67cbf4322a964bef7b3ecd738833faf84
christopherkeibel <christopher.keibel@karakun.com>
2022-04-29 11:03:41 +0200

    add first actions and init reward function

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 
 
@@ -9,14 +11,40 @@ class TicTacToe():
     def reset(self):
         self.env = np.full((3, 3), '')
 
-    def step(self, action: int):
-        pass
+    def step(self, action: int, symbol: str) -> Tuple[bool, int]:
+
+        def reward() -> int:
+            return 1
+
+        if action == 1 and self.env[0][0] == '':
+            self.env[0][0] = symbol
+            return True, reward()
+        if action == 2:
+            self.env[0][1] = symbol
+            return True, reward()
+        else:
+            return False, reward()
+
+
 
 
 if __name__ == "__main__":
     game = TicTacToe()
-    game.env[0][0] = 'X'
+    game.reset()
+    ok, r = game.step(action=1, symbol='X')
+    print(r)
+    print(ok)
     print(game.env)
 
-    game.reset()
+
+    ok, r = game.step(action=2, symbol='O')
+    print(r)
+    print(ok)
     print(game.env)
+
+    ok = game.step(action=1, symbol='O')
+    print(r)
+    print(ok)
+    print(game.env)
+
+
