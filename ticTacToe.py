c43e9377bed010c760139b2191fb83fb386ca3b3
christopherkeibel <christopher.keibel@karakun.com>
2022-04-29 10:19:21 +0200

    init action

--- ticTacToe.py
+++ ticTacToe.py
@@ -4,9 +4,13 @@ import numpy as np
 class TicTacToe():
     def __init__(self):
         self.env = np.full((3, 3), '')
+        self.action_space = 9  # TODO
 
     def reset(self):
         self.env = np.full((3, 3), '')
+
+    def step(self, action: int):
+        pass
 
 
 if __name__ == "__main__":
