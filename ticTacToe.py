163947f3ed5b07474114d80e77d2611918c90797
christopherkeibel <christopher.keibel@karakun.com>
2022-04-29 11:13:39 +0200

    add actions, init done function, add test loop through env

--- ticTacToe.py
+++ ticTacToe.py
@@ -13,38 +13,50 @@ class TicTacToe():
 
     def step(self, action: int, symbol: str) -> Tuple[bool, int]:
 
+        def done() -> bool:
+            pass
+
         def reward() -> int:
             return 1
 
-        if action == 1 and self.env[0][0] == '':
+        if action == 0 and self.env[0][0] == '':
             self.env[0][0] = symbol
             return True, reward()
-        if action == 2:
+        if action == 1:
             self.env[0][1] = symbol
+            return True, reward()
+        if action == 2:
+            self.env[0][2] = symbol
+            return True, reward()
+        if action == 3:
+            self.env[1][0] = symbol
+            return True, reward()
+        if action == 4:
+            self.env[1][1] = symbol
+            return True, reward()
+        if action == 5:
+            self.env[1][2] = symbol
+            return True, reward()
+        if action == 6:
+            self.env[2][0] = symbol
+            return True, reward()
+        if action == 7:
+            self.env[2][1] = symbol
+            return True, reward()
+        if action == 8:
+            self.env[2][2] = symbol
             return True, reward()
         else:
             return False, reward()
 
 
-
-
 if __name__ == "__main__":
     game = TicTacToe()
     game.reset()
-    ok, r = game.step(action=1, symbol='X')
-    print(r)
-    print(ok)
-    print(game.env)
 
-
-    ok, r = game.step(action=2, symbol='O')
-    print(r)
-    print(ok)
-    print(game.env)
-
-    ok = game.step(action=1, symbol='O')
-    print(r)
-    print(ok)
-    print(game.env)
-
-
+    for i in range(9):
+        if i % 2 == 0:
+            game.step(action=i, symbol='X')
+        else:
+            game.step(action=i, symbol='O')
+        print(game.env)
